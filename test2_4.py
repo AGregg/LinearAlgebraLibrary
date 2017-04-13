1b2f3548c743af29f20c8fb9646ddef9f074b87f
Andrew Gregg <andrewggregg@gmail.com>
2017-04-13 00:33:37 -0400

    fix vector_math, update test for 2_4

--- test2_4.py
+++ test2_4.py
@@ -1,5 +1,14 @@
 from vector import *
 from vector_math import *
-x = Vector([8.218,-1.129])
-y = Vector([-9.341,2.111])
+
+x = Vector([8.218,-9.341])
+y = Vector([-1.129,2.111])
 print(vectorAdd(x,y).coordinates)
+
+x = Vector([7.119,8.215])
+y = Vector([-8.223,0.878])
+print(vectorSubtract(x,y).coordinates)
+
+x = Vector([1.671,-1.012,-0.318])
+y = 7.41
+print(scalarMultiply(x,y).coordinates)
--- vector_math.py
+++ vector_math.py
@@ -3,6 +3,23 @@ from vector import *
 def vectorAdd(x, y):
     value = []
     for i in range(len(x.coordinates)):
-        print(i)
         value.append(x.coordinates[i] + y.coordinates[i])
+    return Vector(value)
+
+def vectorSubtract(x, y):
+    value = []
+    for i in range(len(x.coordinates)):
+        value.append(x.coordinates[i] - y.coordinates[i])
+    return Vector(value)
+
+def scalarMultiply(x, y):
+    value = []
+    for i in range(len(x.coordinates)):
+        value.append(x.coordinates[i] * y)
+    return Vector(value)
+
+def vectorNegate(x):
+    value = []
+    for i in range(len(x.coordinates)):
+        value.append(-x.coordinates[i])
     return Vector(value)
